af7a331272de721532c5103c0c3491603139bf95
Manuel Barraud <manuel.barraud@biig.fr>
2018-10-08 12:13:40 +0200

    replace original project by biig-io

--- acme.sh
+++ acme.sh
@@ -6,7 +6,7 @@ PROJECT_NAME="acme.sh"
 
 PROJECT_ENTRY="acme.sh"
 
-PROJECT="https://github.com/Neilpang/$PROJECT_NAME"
+PROJECT="https://github.com/biig-io/$PROJECT_NAME"
 
 DEFAULT_INSTALL_HOME="$HOME/.$PROJECT_NAME"
 _SCRIPT_="$0"
